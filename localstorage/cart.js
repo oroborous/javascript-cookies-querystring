d981b6f0c83e8058fcb0c8e2a9e0a731a9d4803c
Stacy Read <stacy.read.007@gmail.com>
2020-12-06 15:52:27 -0600

    Example of storing array of objects in local storage

--- localstorage/cart.js
+++ localstorage/cart.js
@@ -1,12 +1,45 @@
-$(document).ready(function() {
-   var orderInfo = JSON.parse(localStorage.getItem("order"));
+$(document).ready(function () {
+   displayManyItems();
 
-   var sleeveStyle = orderInfo.style;
-   var size = orderInfo.size;
-   var qty = orderInfo.qty;
+   /*
+   Retrieves a single order object from local storage and creates
+   a table row to display it.
+    */
+    function displaySingleItem() {
+        var orderInfo = JSON.parse(localStorage.getItem("order"));
 
-   $("td#item").text(`${sleeveStyle}-sleeve shirt`);
-   $("td#size").text(size);
-   $("td#qty").text(qty);
+        var sleeveStyle = orderInfo.style;
+        var size = orderInfo.size;
+        var qty = orderInfo.qty;
 
+        var itemCell = $("<td>").text(`${sleeveStyle}-sleeve shirt`);
+        var sizeCell = $("<td>").text(size);
+        var qtyCell = $("<td>").text(qty);
+
+        var tableRow = $("<tr>").append(itemCell, sizeCell, qtyCell);
+
+        $("tbody").append(tableRow);
+    }
+
+    /*
+    Retrieves an array of order objects from local storage and loops
+    to create one table row for each object.
+     */
+    function displayManyItems() {
+        var orderArray = JSON.parse(localStorage.getItem("order"));
+
+        for (var orderInfo of orderArray) {
+            var sleeveStyle = orderInfo.style;
+            var size = orderInfo.size;
+            var qty = orderInfo.qty;
+
+            var itemCell = $("<td>").text(`${sleeveStyle}-sleeve shirt`);
+            var sizeCell = $("<td>").text(size);
+            var qtyCell = $("<td>").text(qty);
+
+            var tableRow = $("<tr>").append(itemCell, sizeCell, qtyCell);
+
+            $("tbody").append(tableRow);
+        }
+    }
 });
--- localstorage/order.js
+++ localstorage/order.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    $("form").submit(addToCart);
+    $("form").submit(addMultipleItems);
 
-    function addToCart() {
+    /*
+    Adds one order object to local storage, to be retrieved
+    by the JS in cart.html
+     */
+    function addSingleItem() {
         var orderInfo = {};
 
         orderInfo.style = $(":radio[name=style]:checked").val();
@@ -9,5 +13,28 @@ $(document).ready(function() {
         orderInfo.qty = parseInt($("#qty").val());
 
         localStorage.setItem("order", JSON.stringify(orderInfo));
+    }
+
+    /*
+    Gets the array of order objects out of local storage and adds
+    the newest object to it.
+     */
+    function addMultipleItems() {
+        var cartContents = JSON.parse(localStorage.getItem("order"));
+
+        if (!cartContents) {
+            cartContents = [];
+        }
+
+        var orderInfo = {};
+
+        orderInfo.style = $(":radio[name=style]:checked").val();
+        orderInfo.size = $("#size").val();
+        orderInfo.qty = parseInt($("#qty").val());
+
+        cartContents.push(orderInfo);
+
+        localStorage.setItem("order", JSON.stringify(cartContents));
+
     }
 });
