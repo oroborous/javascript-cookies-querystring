e28daf0058f789934a0c1a7dc082ac9319567c91
Stacy Read <stacy.read.007@gmail.com>
2020-12-07 21:08:23 -0600

    Fixed bug where cart did not already exist

--- localstorage/cart.js
+++ localstorage/cart.js
@@ -25,19 +25,27 @@ $(document).ready(function () {
     to create one table row for each object.
      */
     function displayManyItems() {
-        var orderArray = JSON.parse(localStorage.getItem("cart"));
+        // Get the JSON string from local storage
+        var orderArray = localStorage.getItem("cart");
 
-        for (var orderInfo of orderArray) {
-            // Create table cells with text inside
-            var itemCell = $("<td>").text(`${orderInfo.style}-sleeve shirt`);
-            var sizeCell = $("<td>").text(orderInfo.size);
-            var qtyCell = $("<td>").text(orderInfo.qty);
+        // Does it exist or is it null?
+        if (orderArray) {
+            // It exists, so parse it back into an array
+            orderArray = JSON.parse(orderArray);
 
-            // Add all the cells to a table row
-            var tableRow = $("<tr>").append(itemCell, sizeCell, qtyCell);
+            // Loop over the array
+            for (var orderInfo of orderArray) {
+                // Create table cells with text inside
+                var itemCell = $("<td>").text(`${orderInfo.style}-sleeve shirt`);
+                var sizeCell = $("<td>").text(orderInfo.size);
+                var qtyCell = $("<td>").text(orderInfo.qty);
 
-            // Add the table row to the tbody
-            $("tbody").append(tableRow);
+                // Add all the cells to a table row
+                var tableRow = $("<tr>").append(itemCell, sizeCell, qtyCell);
+
+                // Add the table row to the tbody
+                $("tbody").append(tableRow);
+            }
         }
     }
 });
